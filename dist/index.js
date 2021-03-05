0d3b20b70c0e0de4fcd51c7ec609973dd1157364
Danny Wei <katsutoxin@gmail.com>
2021-03-05 20:54:13 +0800

    add debug info

--- dist/index.js
+++ dist/index.js
@@ -47,7 +47,7 @@ const { getListOfLabels, commentPr, doesPrHasLabels, getListOfAssignees, getMile
 
     const requiredMilestone = core.getInput('required_milestone');
     const milestone = getMilestone()
-    core.info(`requiredMilestone=${requiredMilestone}`)
+    // core.info(`requiredMilestone=${requiredMilestone}`)
     // core.debug(`milestone=${milestone}`)
     if (requiredMilestone === 'true') {
       if (milestone === null) {
@@ -55,14 +55,14 @@ const { getListOfLabels, commentPr, doesPrHasLabels, getListOfAssignees, getMile
         core.error(errorMsg)
         core.setFailed(errorMsg)
       } else {
-        core.info(`found milestone: ${milestone.title}`)
+        core.info(`Found milestone: ${milestone.title}`)
       }
 
     }
 
     const requiredAssignee = core.getInput('required_assignee');
     const assignees = getListOfAssignees()
-    core.info(`requiredAssignee=${requiredAssignee}`)
+    // core.info(`requiredAssignee=${requiredAssignee}`)
     // core.debug(`assignees=${assignees}`)
     if (requiredAssignee === 'true') {
       if (assignees.length === 0) {
@@ -70,7 +70,7 @@ const { getListOfLabels, commentPr, doesPrHasLabels, getListOfAssignees, getMile
         core.error(errorMsg)
         core.setFailed(errorMsg)
       } else {
-        core.info(`found assignees: ${assignees.map(x=>x.login).join()}`)
+        core.info(`Found assignees: ${assignees.map(x=>x.login).join()}`)
       }
 
     }
